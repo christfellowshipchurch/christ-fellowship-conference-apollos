9cfd1ef0cb2a28d4eb6971e7aafd1e2b7221b14f
Caleb Panza <caleb.panza@cftoday.org>
2018-12-12 13:01:18 -0500

    Update index.js

--- packages/apollos-church-api/src/data/content-items/index.js
+++ packages/apollos-church-api/src/data/content-items/index.js
@@ -7,11 +7,10 @@ export const resolver = {
     ContentItem: {
         ...ContentItem.resolver.ContentItem,
         __resolveType: async (attrs, ...otherProps) => {
-            console.log("Attributes: ",
+            console.log("Has Price: ",
                 Object.hasOwnProperty.call(attrs.attributeValues, 'price'));
 
             if (Object.hasOwnProperty.call(attrs.attributeValues, 'price')) {
-                console.log("Found Event Ticket");
                 return 'EventTicketContentItem';
             }
             return ContentItem.resolver.ContentItem.__resolveType(
