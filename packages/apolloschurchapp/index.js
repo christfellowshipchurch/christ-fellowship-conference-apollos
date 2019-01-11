cebad4ba405378f9957901e0ec7a555efccec25d
Vincent Wilson <vince@classyh.at>
2019-01-11 10:05:48 -0600

    Fix linter

--- packages/apolloschurchapp/index.js
+++ packages/apolloschurchapp/index.js
@@ -1,9 +1,10 @@
 import { AppRegistry, YellowBox } from 'react-native';
 import { Client } from 'bugsnag-react-native';
 
-import { BUGSNAG_API_KEY } from './src/config'
+import { BUGSNAG_API_KEY } from './src/config';
 import App from './src';
 
+// eslint-disable-next-line no-unused-vars
 const bugsnag = new Client(BUGSNAG_API_KEY);
 
 YellowBox.ignoreWarnings([
