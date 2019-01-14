c06a5681ce0f31acc15577dd155f387dbb34e11a
Vincent Wilson <vince@classyh.at>
2019-01-14 10:44:09 -0600

    Adds Bugsnag for error reporting (#3)
    * Add bugsnag for error reporting
    
    * Fix linter

--- packages/apolloschurchapp/index.js
+++ packages/apolloschurchapp/index.js
@@ -1,6 +1,11 @@
 import { AppRegistry, YellowBox } from 'react-native';
+import { Client } from 'bugsnag-react-native';
 
+import { BUGSNAG_API_KEY } from './src/config';
 import App from './src';
+
+// eslint-disable-next-line no-unused-vars
+const bugsnag = new Client(BUGSNAG_API_KEY);
 
 YellowBox.ignoreWarnings([
   'Warning: isMounted(...) is deprecated',
