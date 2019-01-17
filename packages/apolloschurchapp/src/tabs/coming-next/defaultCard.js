33e9433da81596ae049eb77e969d9ace7cbf2a3a
Caleb Panza <caleb.panza@icloud.org>
2019-01-17 17:51:46 -0500

    Added Coming Next View

---
+++ packages/apolloschurchapp/src/tabs/coming-next/defaultCard.js
@@ -0,0 +1,15 @@
+export default {
+  userFeed: {
+    edges: [
+      {
+        node: {
+          __typename: 'UniversalContentItem',
+          id: 'UniversalContentItem:DefaultComingNextCard',
+          title: 'More Coming Soon!',
+          htmlContent:
+            'Make sure to check back here frequently to stay up to date with everything going on at Christ Fellowship Conference 2019.',
+        },
+      },
+    ],
+  },
+};
---
+++ packages/apolloschurchapp/src/tabs/coming-next/getUserFeed.js
@@ -0,0 +1,19 @@
+import gql from 'graphql-tag';
+
+import { contentItemFragment } from 'apolloschurchapp/src/content-single/getContentItem';
+import { largeCardFragment } from 'apolloschurchapp/src/ui/ContentCardConnected';
+
+export default gql`
+  query getUserFeed {
+    userFeed(first: 1) {
+      edges {
+        node {
+          ...largeCardFragment
+          ...contentItemFragment
+        }
+      }
+    }
+  }
+  ${contentItemFragment}
+  ${largeCardFragment}
+`;
