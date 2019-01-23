5ae2203b12251789fb37b957dff4b47320f74e74
calebpanza <45076058+calebpanza@users.noreply.github.com>
2019-01-23 15:25:07 -0500

    Cfcw 62 tab bar top padding (#25)
    * Added additional padding to tabBar
    
    * Made the tab icons smaller

--- packages/apolloschurchapp/src/tabs/tabBar.js
+++ packages/apolloschurchapp/src/tabs/tabBar.js
@@ -35,6 +35,7 @@ const ThemedBottomTabBar = compose(
   styled(({ theme }) => ({
     borderTopWidth: 0,
     backgroundColor: theme.colors.transparent,
+    paddingTop: 10,
   }))
 )(BottomTabBar);
 
