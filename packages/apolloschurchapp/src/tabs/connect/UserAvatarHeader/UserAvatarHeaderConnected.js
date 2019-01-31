c58318fbd3dafedc2c7329085a2b369bac8f2ba4
Vincent Wilson <vince@classyh.at>
2019-01-31 16:47:33 -0600

    Pass guid into url for breakout view (#43)

--- packages/apolloschurchapp/src/tabs/connect/UserAvatarHeader/UserAvatarHeaderConnected.js
+++ packages/apolloschurchapp/src/tabs/connect/UserAvatarHeader/UserAvatarHeaderConnected.js
@@ -34,13 +34,13 @@ const featuresMap = {
       />
     </QRCodeContainer>
   ),
-  'my-breakouts': () => (
+  'my-breakouts': ({ rockGuid }) => (
     <CallToActionContainer>
       <CallToAction
         icon="list"
         title="my breakouts"
         callout="Sign up for or check in to your breakouts"
-        url="https://my.christfellowshipconference.com/mybreakouts"
+        url={`https://my.christfellowshipconference.com/mybreakouts?person=${rockGuid}`}
         useCookie
       />
     </CallToActionContainer>
@@ -62,6 +62,7 @@ const UserAvatarHeaderConnected = ({ navigation }) => (
     {({
       data: {
         currentUser: {
+          rockGuid = '',
           profile: {
             photo,
             firstName,
@@ -91,7 +92,7 @@ const UserAvatarHeaderConnected = ({ navigation }) => (
           .map((feature) => featuresMap[feature])
           .map((Component, i) => (
             // eslint-disable-next-line react/no-array-index-key
-            <Component key={`${i}`} />
+            <Component key={`${i}`} rockGuid={rockGuid} />
           ))}
       </React.Fragment>
     )}
