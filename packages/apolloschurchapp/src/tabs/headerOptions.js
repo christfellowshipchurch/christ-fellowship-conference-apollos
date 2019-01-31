67df4d67189e5d049b0d598f74cda84eb560e247
Vincent Wilson <vince@classyh.at>
2019-01-31 14:39:47 -0600

    Remove unused View import

--- packages/apolloschurchapp/src/tabs/headerOptions.js
+++ packages/apolloschurchapp/src/tabs/headerOptions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { styled } from '@apollosproject/ui-kit';
-import { Image, View } from 'react-native';
+import { Image } from 'react-native';
 import NavigationService from '../NavigationService';
 
 const LogoTitle = styled(({ theme }) => ({
