ae2a96f1845d5c3bacaa4eae46f7c5d57f74de5b
Caleb Panza <caleb.panza@icloud.org>
2019-02-05 12:05:05 -0500

    Scheduled Profile Elements

--- packages/apollos-church-api/src/data/rock-people/resolver.js
+++ packages/apollos-church-api/src/data/rock-people/resolver.js
@@ -1,4 +1,45 @@
 import { Person } from '@apollosproject/data-connector-rock';
+import moment from 'moment';
+
+const availableFeatures = [
+  {
+    key: 'my-breakouts',
+    startTime: null,
+    endTime: null,
+    order: 1,
+  },
+  {
+    key: 'qr-code',
+    startTime: '2019-02-06 07:30:00-05:00',
+    endTime: '2019-02-06 10:30:00-05:00',
+    order: 2,
+  },
+  {
+    key: 'survey',
+    startTime: '2019-02-07 20:00:00-05:00',
+    endTime: '2019-02-10 12:00:00-05:00',
+    order: 0,
+  },
+];
+
+const nowIsBetweenTimes = (start, end) => {
+  const mStart = moment.utc(start);
+  const mEnd = moment.utc(end);
+  const now = moment.utc();
+
+  const afterStart = now.isSameOrAfter(mStart) || !start;
+  const beforeEnd = now.isSameOrBefore(mEnd) || !end;
+
+  return afterStart && beforeEnd;
+};
+
+const selectAvailableFeatures = () => {
+  const active = availableFeatures
+    .filter((n) => nowIsBetweenTimes(n.startTime, n.endTime))
+    .sort((a, b) => a.order - b.order);
+
+  return active.map((n) => n.key);
+};
 
 export default {
   ...Person.resolver,
@@ -35,9 +76,6 @@ export default {
       typeof attributeValues.instagram === 'object'
         ? attributeValues.instagram.value
         : '',
-    // activeFeatures: () => [],
-    activeFeatures: () => ['my-breakouts'],
-    // activeFeatures: () => ['my-breakouts', 'qr-code'],
-    // activeFeatures: () => ['my-breakouts', 'survey', 'qr-code'],
+    activeFeatures: () => selectAvailableFeatures(),
   },
 };
