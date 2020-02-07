eca265cac01daa7af95e9eb96506948e4f18c569
Caleb Panza <caleb.panza@icloud.org>
2020-02-07 12:51:46 -0500

    Caching updates
    Updated caches to make sure that all necessary requests are reset on logout
    Updated react apollo packages

--- packages/apolloschurchapp/src/client/index.js
+++ packages/apolloschurchapp/src/client/index.js
@@ -5,20 +5,37 @@ import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import SplashScreen from 'react-native-splash-screen';
 import gql from 'graphql-tag';
+import DeviceInfo from 'react-native-device-info';
 
+import { resolvers, schema, defaults } from '../store';
 import httpLink from './httpLink';
 import clientStateLink from './clientStateLink';
 import authLink from './authLink'; // eslint-disable-line
 import cache, { ensureCacheHydration } from './cache';
 
 const link = clientStateLink.concat(ApolloLink.from([authLink, httpLink]));
+const wipeData = () => cache.writeData({ data: defaults });
+
+let clearStore;
 
 export const client = new ApolloClient({
   link,
   cache,
   queryDeduplication: true,
   shouldBatch: true,
+  resolvers,
+  typeDefs: schema,
+  name: DeviceInfo.getApplicationName(),
+  version: DeviceInfo.getVersion(),
 });
+
+// Hack to give auth link access to method on client;
+// eslint-disable-next-line prefer-destructuring
+clearStore = client.clearStore;
+
+wipeData();
+// Ensure that media player still works after logout.
+client.onClearStore(() => wipeData());
 
 client.onResetStore(clientStateLink.writeDefaults);
 
--- packages/apolloschurchapp/src/tabs/connect/MyBreakouts/index.js
+++ packages/apolloschurchapp/src/tabs/connect/MyBreakouts/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
 import { Query } from 'react-apollo';
 import { withNavigation } from 'react-navigation';
 import { get } from 'lodash';
@@ -12,6 +18,7 @@ import {
   TouchableScale,
   styled,
   Button,
+  FeedView,
 } from '@apollosproject/ui-kit';
 import ContentCardConnected from '../../../ui/ContentCardConnected';
 import { ThinCard } from '../../../ui/Cards';
@@ -21,7 +28,7 @@ import { MY_BREAKOUTS, BREAKOUTS_SIGN_UP_URL } from './queries';
 
 const Title = styled(({ theme }) => ({
   paddingHorizontal: theme.sizing.baseUnit,
-  paddingBottom: theme.sizing.baseUnit,
+  paddingBottom: theme.sizing.baseUnit * 0.25,
   borderBottomWidth: StyleSheet.hairlineWidth,
   borderColor: theme.colors.darkPrimary,
 }))(H3);
@@ -51,6 +58,13 @@ const BreakoutReloadButton = styled(({ theme }) => ({
 
 const loadingTheme = { colors: { primary: 'rgba(0, 0, 0, 0.35)' } };
 
+const BreakoutCard = ({ id, times, ...props }) => (
+  <BreakoutContainer>
+    <BreakoutSessionTitle>{get(times, '[0].value')}</BreakoutSessionTitle>
+    <ContentCardConnected contentId={id} card={ThinCard} {...props} />
+  </BreakoutContainer>
+);
+
 const MyBreakoutList = ({ navigation, breakouts, loading }) =>
   loading ? (
     <BreakoutContainer>
@@ -59,40 +73,31 @@ const MyBreakoutList = ({ navigation, breakouts, loading }) =>
       <ThinCard isLoading theme={loadingTheme} />
     </BreakoutContainer>
   ) : (
-      <View>
+      <FlexedView>
         <Title>My Breakouts</Title>
-        {breakouts.map(({ id, times, theme, ...props }, i) => (
-          <BreakoutContainer key={i}>
-            <BreakoutSessionTitle>{get(times, '[0].value')}</BreakoutSessionTitle>
-            <TouchableScale
-              onPress={() =>
-                navigation.push('ContentSingle', {
-                  itemId: id,
-                  headerTitle: 'My Breakouts',
-                  theme: {
-                    colors: {
-                      primary: 'white',
-                      text: {
-                        primary: Color('white').fade(0.25),
-                      },
-                      background: {
-                        paper: get(theme, 'colors.primary', 'white'),
-                      },
-                    },
+        <FeedView
+          ListItemComponent={BreakoutCard}
+          content={breakouts}
+          isLoading={loading}
+          onPressItem={({ id, theme }) =>
+            navigation.push('ContentSingle', {
+              itemId: id,
+              headerTitle: 'My Breakouts',
+              theme: {
+                colors: {
+                  primary: 'white',
+                  text: {
+                    primary: Color('white').fade(0.25),
                   },
-                })
-              }
-            >
-              <ContentCardConnected
-                key={i}
-                contentId={id}
-                card={ThinCard}
-                {...props}
-              />
-            </TouchableScale>
-          </BreakoutContainer>
-        ))}
-      </View>
+                  background: {
+                    paper: get(theme, 'colors.primary', 'white'),
+                  },
+                },
+              },
+            })
+          }
+        />
+      </FlexedView>
     );
 
 MyBreakoutList.propTypes = {
@@ -120,8 +125,9 @@ const MyBreakoutSignUp = ({ isLoading }) => (
 );
 
 const MyBreakouts = ({ navigation }) => (
-  <Query query={MY_BREAKOUTS} fetchPolicy="cache-and-network">
+  <Query query={MY_BREAKOUTS} fetchPolicy="network-only">
     {({ loading, data, refetch }) => {
+      if (loading) return <ActivityIndicator />;
       const myBreakouts = get(data, 'myBreakouts', []);
 
       return myBreakouts.length ? (
